3ba6ce749decc164149d14d6befe9881036548ea
manikondaHasya <hasya.manikonda06@gmail.com>
2025-05-06 16:10:19 +0530

    Update README.js

--- README.js
+++ README.js
@@ -1,32 +1,6 @@
-# Responsive Landing Page with Interactive Navigation
 
-A responsive landing page featuring a navigation menu that changes on scroll and hover.
+## Git/GitHub Workflow
 
-## Features
-
-- Fixed position navigation bar
-- Color change on scroll
-- Hover effects on menu items
-- Mobile-responsive hamburger menu
-- Smooth scrolling to sections
-- Active section highlighting
-
-## Technologies Used
-
-- HTML5
-- CSS3
-- JavaScript
-
-## How to Use
-
-1. Clone this repository
-2. Open `index.html` in your browser
-3. Scroll to see the navigation effect
-4. Hover over menu items to see the animation
-5. On mobile, click the hamburger icon to toggle the menu
-
-## Installation
-
+### 1. Initialize Git Repository
 ```bash
-git clone https://github.com/your-username/responsive-landing-page.git
-cd responsive-landing-page
+git init
